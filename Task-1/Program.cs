49b7a7f193e6b882f6aa8d3f08d74d72714c3de9
NikaEbraLidze <nikaebralidze21@gmail.com>
2025-09-22 19:48:55 +0400

    homework 3

--- Task-1/Program.cs
+++ Task-1/Program.cs
@@ -4,186 +4,143 @@ class Program
 {
     static void Main()
     {
-        // 1. რიცხვების ბეჭდვა (Print Numbers 1–10)
-        // დავალება: დაბეჭდე რიცხვები 1-იდან 10-მდე for ციკლით.
-        // · Input: არაფერი
-        // · Output: 1 2 3 4 5 6 7 8 9 10
+        // დავალება 1
 
-        // for (int i = 1; i <= 10; i++)
+        // დაწერეთ პროგრამა რომელიც გამოიტანს კონსოლში ფიფქებით შედგენილ ნახევარპირამიდის ფორმას.
+        // მაგალითად ციფრი 4–ის შეყვანისას კონსოლში გამოვა შემდეგი სახის ნახევარ პირამიდა:
+        // *
+        // **
+        // ***
+        // ****
+
+
+
+
+
+
+        // try
         // {
-        //     Console.Write(i + " ");
-        // }
+        //     Console.Write("შეიყვანეთ რიცხვი: ");
+        //     int n = int.Parse(Console.ReadLine());
 
-
-
-        //2. რიცხვების ჯამი (Sum of Numbers 1–100)
-        // დავალება: იპოვე რიცხვების ჯამი 1-დან 100-მდე.
-        // · Input: არაფერი
-        // · Output: "Sum = 5050"
-
-        // int sum = 0;
-        // for (int i = 1; i <= 100; i++)
-        // {
-        //     sum += i;
-        //     if (i == 100)
-        //         Console.Write("Sum = " + sum);
-        // }
-
-
-
-        // 3.კენტი რიცხვების ბეჭდვა(Odd Numbers)
-        // დავალება: დაბეჭდე 1 - დან 20 - მდე ყველა კენტი რიცხვი.
-        // · Input: არაფერი
-        // · Output: 1 3 5 7... 19
-
-        // for (int i = 1; i < 20; i += 2)
-        // {
-        //     Console.Write(i + " ");
-        // }
-
-
-
-        // 4. რიცხვის ტაბულა (Multiplication Table)
-        // დავალება: დაბეჭდე 1 დან 9 მდე გამრავლების ტაბულა.
-
-        // for (int i = 1; i < 10; i++)
-        // {
-        //     for (int b = 1; b < 10; b++)
+        //     if (n <= 0)
         //     {
-        //         Console.WriteLine($"{i} * {b} = {i * b}");
+        //         throw new Exception("რიცხვი უნდა იყოს დადებითი!");
         //     }
-        //     Console.WriteLine();
+
+        //     // ციკლი ნახევარპირამიდისთვის
+        //     for (int i = 1; i <= n; i++)
+        //     {
+        //         for (int j = 1; j <= i; j++)
+        //         {
+        //             Console.Write("*");
+        //         }
+        //         Console.WriteLine();
+        //     }
         // }
-
-        // 5. უკუღმა ბეჭდვა (Reverse Numbers)
-        // დავალება: დაბეჭდე რიცხვები 10-იდან 1-მდე.
-        // Input: არაფერი
-        // · Output: 10 9 8 ... 1
-
-        // for (int i = 10; i >= 1; i--)
+        // catch
         // {
-        //     Console.Write(i + " ");
-        // }
-
-
-
-        // 6. რიცხვის ფაქტორიალი (Factorial)
-        // დავალება: გამოთვალე რიცხვის ფაქტორიალი n!.
-        // · Input: n = 5
-        // · Output: "Factorial = 120"
-
-        // Console.Write("Enter Number: ");
-
-        // int num = int.Parse(Console.ReadLine());
-        // int factorial = 1;
-
-        // for (int i = 1; i <= num; i++)
-        // {
-        //     factorial *= i;
-        // }
-
-        // Console.WriteLine($"Factorial = {factorial}");
-
-
-
-        // 7. სიმბოლოს გამეორება (Repeat Character)
-        // დავალება: დაბეჭდე "*" სიმბოლო 10-ჯერ ერთ ხაზზე.
-        // · Input: არაფერი
-        // · Output: "**********"
-
-        // for (int i = 1; i <= 10; i++)
-        // {
-        //     Console.Write("* ");
+        //     Console.WriteLine("შეცდომა მოხდა! შეამოწმეთ შეყვანილი მონაცემები.");
         // }
 
 
 
-        // 8. While ციკლი – ჯამი სანამ არ გადავაჭარბებთ
-        // დავალება: დაამატე რიცხვები სანამ ჯამი არ გახდება ≥ 50.
-        // · Input: არაფერი
-        // · Output: "Sum = 55"
 
-        // int sum = 0;
-        // int i = 0;
 
-        // while (sum <= 50)
+        // დავალება 2
+        // მომხმარებელს კლავიატურის გამოყენებით შეჰყავს 2 რიცხვი. ჩვენი ამოცანაა, რომ ვაჩვენოთ ყველა
+        // ლუწი რიცხვი მითითებულ დიაპაზონში. თუ დიაპზონის საზღვრები არასწორად არის მოცემული, თქვენ უნდა
+        // ჩაასწოროთ ის. მაგალითად, თუ მომხმარებელმა შეიყვანა 20 და 11, ჩასწორებაა საჭირო, რადგან დიაპაზონის
+        // დასაწყისი უნდა იყოს - 11, და დასასრული - 20.
+
+        // try
         // {
-        //     sum += i;
-        //     i++;
+        //     Console.Write("შეიყვანეთ პირველი რიცხვი: ");
+        //     int a = int.Parse(Console.ReadLine());
+
+        //     Console.Write("შეიყვანეთ მეორე რიცხვი: ");
+        //     int b = int.Parse(Console.ReadLine());
+
+        //     // თუ დიაპაზონი არასწორადაა, შევცვალოთ
+        //     if (a > b)
+        //     {
+        //         int temp = a;
+        //         a = b;
+        //         b = temp;
+        //     }
+
+        //     Console.WriteLine($"ლუწი რიცხვები {a}-დან {b}-მდე:");
+        //     for (int i = a; i <= b; i++)
+        //     {
+        //         if (i % 2 == 0)
+        //         {
+        //             Console.Write(i + " ");
+        //         }
+        //     }
+        // }
+        // catch
+        // {
+        //     Console.WriteLine("შეცდომა მოხდა! შეამოწმეთ შეყვანილი მონაცემები.");
         // }
 
-        // Console.WriteLine($"Sum = {sum}");
 
 
 
-        // 9. Do-While ციკლი – პაროლი
-        // დავალება: პროგრამამ პაროლი უნდა მოითხოვოს მანამდე, სანამ სწორს არ შეიყვანს.
-        // · Input: "1234" (სწორი პაროლი)
-        // · Output: "Enter password: ... Correct!"
-
-        // Console.Write("Enter Password: ");
-        // string password = Console.ReadLine();
-
-        // do
-        // {
-        //     Console.Write("Enter Password: ");
-        //     password = Console.ReadLine() ?? "";
-        // } while (password != "1234");
-
-        // Console.WriteLine("Correct!");
 
 
-
-        // 12. რიცხვის პალინდრომი (Palindrome Number Check)
-        // დავალება: შეამოწმე, არის თუ არა რიცხვი პალინდრომი (იგივე წაკითხვით წინ და უკან).
-        // · Input: 121
-        // · Output: "Palindrome"
-        // · Input: 123
-        // · Output: "Not Palindrome"
-
-        // Console.Write("Enter Password: ");
-        // string password = Console.ReadLine();
+        // Exception
+        // · კალკულატორის დავალება გადააწყეთ ისე რომ გამოიყენოთ Exception Handling, Try Catch და Throw
 
 
-        // Console.Write("Enter a number: ");
-        // int num = int.Parse(Console.ReadLine());
-        // int original = num;
-        // int reversed = 0;
+        bool correctInput = false; // სანამ ეს არ გახდება true, ციკლი იმუშავებს
 
-        // while (num > 0)
-        // {
-        //     int digit = num % 10;
-        //     reversed = reversed * 10 + digit;
-        //     num /= 10;
-        // }
+        while (!correctInput)
+        {
+            try
+            {
+                Console.Write("შეიყვანეთ პირველი რიცხვი: ");
+                double num1 = double.Parse(Console.ReadLine());
 
-        // if (reversed == original)
-        //     Console.WriteLine("Palidrome");
-        // else
-        //     Console.WriteLine("Not Palidrome");
+                Console.Write("შეიყვანეთ მოქმედება (+, -, *, /): ");
+                string op = Console.ReadLine();
 
+                Console.Write("შეიყვანეთ მეორე რიცხვი: ");
+                double num2 = double.Parse(Console.ReadLine());
 
-        // 13. რიცხვების ფიბონაჩის მიმდევრობა (Fibonacci Sequence)
-        // დავალება: დაბეჭდე პირველი n ელემენტი ფიბონაჩის მიმდევრობიდან.
-        // · Input: n = 7
-        // · Output: 0 1 1 2 3 5 8
+                double result = 0;
 
+                if (op == "+")
+                {
+                    result = num1 + num2;
+                }
+                else if (op == "-")
+                {
+                    result = num1 - num2;
+                }
+                else if (op == "*")
+                {
+                    result = num1 * num2;
+                }
+                else if (op == "/")
+                {
+                    if (num2 == 0)
+                    {
+                        throw new Exception("ნულზე გაყოფა აკრძალულია!");
+                    }
+                    result = num1 / num2;
+                }
+                else
+                {
+                    throw new Exception("არასწორი ოპერაცია!");
+                }
 
-        // Console.Write("Enter a number: ");
-        // int num = int.Parse(Console.ReadLine());
-
-        // int lastLastNum = 0;
-        // int lastNum = 1;
-
-        // Console.Write($"{lastLastNum} {lastNum} ");
-
-        // for (int i = 2; i < num; i++)
-        // {
-        //     int nextNum = lastLastNum + lastNum;
-        //     Console.Write(nextNum + " ");
-
-        //     lastLastNum = lastNum;
-        //     lastNum = nextNum;
-        // }
+                Console.WriteLine("შედეგი: " + result);
+                correctInput = true; // თუ აქამდე მოვედით, ყველაფერი სწორადაა => ციკლი მთავრდება
+            }
+            catch
+            {
+                Console.WriteLine("შეცდომა! სცადეთ თავიდან.\n");
+            }
+        }
     }
 }
